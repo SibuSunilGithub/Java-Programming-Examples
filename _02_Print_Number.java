d0d9c2fff962ddf1596f9de2e0424354b96be899
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-15 00:20:26 +0530

    Update and rename PrintNumber.java to _02_Print_Number.java
    name change

--- _02_Print_Number.java
+++ _02_Print_Number.java
@@ -1,9 +1,9 @@
-// WAP to print a number enter by the user ...
+// WAP to print a number entered by the user ...
 
 import java.util.Scanner;
 
 
-public class PrintNumber {
+public class _02_Print_Number {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
             System.out.println("Enter a number:");
