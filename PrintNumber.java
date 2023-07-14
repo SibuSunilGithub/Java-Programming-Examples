837b0a275b426ba4317c81ccbf07ee9c31bb0048
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-14 23:58:01 +0530

    Print A Number Enter By User

---
+++ PrintNumber.java
@@ -0,0 +1,15 @@
+// WAP to print a number enter by the user ...
+
+import java.util.Scanner;
+
+
+public class PrintNumber {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number:");
+            int num= sc.nextInt();
+            System.out.println("You entered:"+num);
+        }
+
+    }
+
