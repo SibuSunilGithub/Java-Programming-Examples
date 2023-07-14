dd07410b613f3147c77e57e23245a587384dc130
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-15 00:18:00 +0530

    Update and rename HelloWorld.java to _01_hello_world.java
    change name

--- _01_hello_world.java
+++ _01_hello_world.java
@@ -1,6 +1,6 @@
-// Write a program to print Hello, World on screen...
+// Write a program to print Hello, World on the screen...
 
-public class HelloWorld {
+public class _01_hello_world {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
