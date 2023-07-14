d9b1e1868620bf8e09e4b5af5624d157029ac2ae
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-14 23:59:31 +0530

    Addition Of Two Number

---
+++ Addition.java
@@ -0,0 +1,14 @@
+// WAP to add two numbers...
+
+import java.util.Scanner;        
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number:");
+        int num2 = sc.nextInt();
+        int ans = num1+num2;
+        System.out.println("Addition of two number is:\n"+ans);
+    }
+}
