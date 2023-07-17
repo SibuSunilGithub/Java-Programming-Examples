0f0caf9ff4e6645bf311747a275aa74dce64967c
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-18 00:00:13 +0530

    Update and rename ArithmeticOperations.java to _04_ArithmeticOperations.java
    add rename

--- _04_ArithmeticOperations.java
+++ _04_ArithmeticOperations.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class ArithmeticOperations {
+public class _04_ArithmeticOperations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first number:");
